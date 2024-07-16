e729d7da2a16bb28393eb81e9ddefc6c8320b33b
arthur-augusto <arthur.augusto.rp@gmail.com>
2024-07-15 21:35:38 -0300

    Marcando arestas, true = é linha de trem, false = é uma rodovia

--- 449B/main.cpp
+++ 449B/main.cpp
@@ -3,7 +3,9 @@
 using namespace std;
 
 // Encontra o menor caminho do primeiro nó até todos os outros nós do grafo
-void dijkstra(vector<pair<unsigned int, long long>> adj[], priority_queue<pair<long long, unsigned int>> &q, vector<long long> &distance, vector<bool> &processed) {
+int dijkstra(vector<tuple<int, unsigned long long, bool>> adj[], priority_queue<pair<unsigned long long, int>, vector<pair<unsigned long long, int>>, greater<pair<unsigned long long, int>> > &q, vector<unsigned long long> &distance, vector<bool> &processed) {
+    int train_routes_used = 0;
+
     distance[1] = 0;
     q.push({0, 1});
 
@@ -16,52 +18,50 @@ void dijkstra(vector<pair<unsigned int, long long>> adj[], priority_queue<pair<l
         processed[a] = true;
 
         for (auto u : adj[a]) {
-            int b = u.first, w = u.second;
+            int b = get<0>(u);
+            unsigned long long w = get<1>(u);
+            bool tr = get<2>(u);
 
             if (distance[a] + w < distance[b]) {
                 distance[b] = distance[a] + w;
-                q.push({-distance[b], b});
+                q.push({distance[b], b});
+
+                if (tr) train_routes_used++;
             }
         }
     }
+
+    return train_routes_used;
 }
 
 int main() {
-    unsigned int n, m, k, u, v, x, s, y;
+    int n, m, k, u, v, s;
+    unsigned long long x, y;
 
     cin >> n >> m >> k;
 
-    vector<pair<unsigned int, long long>> adj[n + 1];
+    vector<tuple<int, unsigned long long, bool>> adj[n + 1];
 
-    priority_queue<pair<long long, unsigned int>> q;
+    priority_queue<pair<unsigned long long, int>, vector<pair<unsigned long long, int>>, greater<pair<unsigned long long, int>> > q;
 
-    vector<long long> distance1(n + 1, LLONG_MAX), distance2(n + 1, LLONG_MAX);
-    vector<bool> processed1(n + 1, false), processed2(n + 1, false);
+    vector<unsigned long long> distance(n + 1, ULLONG_MAX);
+    vector<bool> processed(n + 1, false);
 
-    for (unsigned int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         cin >> u >> v >> x;
 
-        adj[u].push_back({v, x});
-        adj[v].push_back({u, x});
+        adj[u].push_back(make_tuple(v, x, false));
+        adj[v].push_back(make_tuple(u, x, false));
     }
 
-    dijkstra(adj, q, distance1, processed1);
-
-    for (unsigned int i = 0; i < k; i++) {
+    for (int i = 0; i < k; i++) {
         cin >> s >> y;
 
-        adj[1].push_back({s, y});
-        adj[s].push_back({1, y});
+        adj[1].push_back(make_tuple(s, y, true));
+        adj[s].push_back(make_tuple(1, y, true));
     }
 
-    dijkstra(adj, q, distance2, processed2);
-
-    unsigned int train_routes_used = 0;
-
-    for (unsigned int i = 1; i <= n; i++) {
-        if (distance2[i] < distance1[i])
-            train_routes_used++;
-    }
+    int train_routes_used = dijkstra(adj, q, distance, processed);
 
     cout << k - train_routes_used << endl;
 }
